db904d511cfdaee32c2856c4c91325d3e7d48525
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-03-07 14:32:55 -0300

    navigation bar for desktop

--- static/js/components.js
+++ static/js/components.js
@@ -1,1 +1,30 @@
-Vue.component()
+// LINKS AND BUTTONS
+Vue.component('white-link', {
+  props: {
+    to: String
+  },
+  template: `
+    <a class='white-link' :href='to'><slot></slot></a>
+  `
+})
+
+Vue.component('navigation', {
+  template: `
+    <div>
+      <div id='navigation' class='card'>
+        <div>
+          <white-link to='/'>Home</white-link>
+          <white-link>Login</white-link>
+          <white-link>User page</white-link>
+        </div>
+        <div>
+        </div>
+        <div>
+          <white-link>Terms of use</white-link>
+          <white-link>Privacy policy</white-link>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  `
+})
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,17 @@
+const MAX_WIDTH_MOBILE_NAVIGATION_BAR = 796
+
 let vm = new Vue({
   el: '#vue',
   data: {
-    working: 'asdfasdf'
-  }
+    desktopLength: undefined
+  },
+  computed: {
+    desktop() {
+      return (this.desktopLength >= MAX_WIDTH_MOBILE_NAVIGATION_BAR)
+    },
+  },
 })
+
+
+vm.desktopLength = window.innerWidth
+window.addEventListener('resize', () => vm.desktopLength = window.innerWidth)
