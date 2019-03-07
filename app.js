5199c0b829cbad40d0daca020331c27700631f17
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-03-07 12:45:01 -0300

    ejs and login view

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let express = require('express')
+let ejs = require('ejs')
 let path = require('path')
 
 let app = express()
 
+let authentication = require('./routes/authentication')
 
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname,'views'))
 app.use(express.static(path.join(__dirname,'static')))
 
+
+app.use('/', authentication)
 
 
 app.get('/', (req, res) => {
