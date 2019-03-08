f21fff73bf1f565326dff31b4d06d8b43e5046e8
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-03-07 23:15:30 -0300

    input form password type

--- static/js/components/forms.js
+++ static/js/components/forms.js
@@ -12,50 +12,14 @@ Vue.component('alert', {
   `,
 })
 
-Vue.component('form-input', {
-  props: {
-    max: Number,
-    placeholder: String,
-    name: String,
-  },
-  data() {
-    return {
-      val: '',
-      empty: undefined,
-      error: undefined,
-      errorType: undefined,
-    }
-  },
-  template: `
-    <div class='form-input form-el'>
-      <div>
-        <input v-model='val' :class='{ input: true, round: true, "wrong-input": error}' autocomplete='off' type='text' :placeholder='placeholder' :name='name' />
-        <alert v-if='error && errorType === "empty"'>
-          This field cannot be empty.
-        </alert>
-        <alert v-if='error && errorType === "max"'>
-          The field has to be less than {{ max + 1 }} characters.
-        </alert>
-      </div>
-    </div>
-  `,
-  watch: {
-    val() {
-      if (this.val === '') {
-        this.error = true
-        this.errorType = 'empty'
-      } else if (this.val.length > this.max) {
-        this.error = true
-        this.errorType = 'max'
-      }
-      else this.error = false
-    }
-  },
-})
-
 Vue.component('card-form', {
   props: {
     act: String
+  },
+  data() {
+    return {
+      map: new Map()
+    }
   },
   template: `
     <div class='card-form'>
@@ -64,4 +28,78 @@ Vue.component('card-form', {
       </div>
     </div>
   `,
+  watch: {
+    map() {
+      console.log(this.map.values())
+    }
+  }
+})
+
+Vue.component('form-input', {
+  props: {
+    max: Number,
+    placeholder: String,
+    name: String,
+    type: String,
+  },
+  data() {
+    return {
+      val: '',
+      empty: undefined,
+      error: undefined,
+      errorType: undefined,
+      showing: true,
+      isPassword: undefined,
+    }
+  },
+  mounted() {
+    if (this.type === 'password') this.showing = false
+    else this.showing = true
+    this.isPassword = !this.showing
+  },
+  template: `
+    <div class='form-input form-el'>
+      <div>
+        <input v-model='val' :class='{ input: true, round: true, "wrong-input": error}' autocomplete='off' :type='inputType' :placeholder='placeholder' :name='name' />
+        <alert v-if='error && errorType === "empty"'>
+          This field cannot be empty.
+        </alert>
+        <alert v-if='error && errorType === "max"'>
+          The field has to be less than {{ max + 1 }} characters.
+        </alert>
+        <template v-if='type === "password"'>
+          <i v-if='showing' @click='showing = false' class='fa fa-eye tine-icon'></i>
+          <i v-if='!showing' @click='showing = true' class='fa fa-eye-slash tine-icon'></i>
+        </template>
+      </div>
+    </div>
+  `,
+  computed: {
+    inputType() {
+      if (this.showing) return "text"
+      else return "password"
+    },
+  },
+  watch: {
+    val() {
+      let obj = { 
+        hasError: true,
+        value: this.val,
+        isPassword: this.isPassword,
+      }
+      if (this.val === '') {
+        this.error = true
+        this.errorType = 'empty'
+      } else if (this.val.length > this.max) {
+        this.error = true
+        this.errorType = 'max'
+      }
+      else {
+        obj.hasError = false
+        this.error = false
+      }
+      this.$parent.map.set(this.name, obj)
+      console.log(this.$parent.map.get(this.name))
+    }
+  },
 })
