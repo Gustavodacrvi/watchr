f24c23366c050a36740165911965cbbeddd2e6a4
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-04-14 19:03:21 -0300

    removed unecessary stuff

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>I am alive</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+
+    };
+  },
+};
+
+</script>
+
+
+
+<style scoped>
+</style>
