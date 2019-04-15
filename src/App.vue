af7d6b455ffdc7dc4a2c2300d92e378c91058e83
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-04-15 19:49:14 -0300

    lint errors

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 import NavBar from './components/Nav-bar.vue';
 
 export default Vue.extend({
   components: {
     'nav-bar': NavBar,
-  }
-})
+  },
+});
 </script>
 
 
