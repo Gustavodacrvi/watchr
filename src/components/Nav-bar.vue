77547e396f5af1fd6040cd0498ba77cba293cb0f
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-04-16 17:25:09 -0300

    Nav-link component

--- src/components/Nav-bar.vue
+++ src/components/Nav-bar.vue
@@ -3,6 +3,7 @@
     <section>
     </section>
     <section>
+      <nav-link> fdaasdf</nav-link>
     </section>
     <section>
     </section>
@@ -11,7 +12,12 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import NavLink from './Nav-link.vue';
+
 export default Vue.extend({
+  components: {
+    'nav-link': NavLink,
+  },
 });
 </script>
 
@@ -21,6 +27,7 @@ nav {
   width: 90%;
   height: 40px;
   padding: 0 5%;
+  position: relative;
 
   display: flex;
   align-items: center;
---
+++ src/components/Nav-link.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <span>
+      <slot></slot>
+    </span>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+export default Vue.extend({
+});
+</script>
+
+<style scoped>
+
+div {
+  position: absolute;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+}
+
+span {
+  padding: 9px;
+}
+
+</style>
