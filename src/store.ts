d0e94f058063d5d27edf0325e5c40a910a5a1c69
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-04-16 14:06:09 -0300

    .state.styles interface

--- src/store.ts
+++ src/store.ts
@@ -3,18 +3,32 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+interface Styles {
+  currentTheme: string;
+  light: {
+    card: {
+      backgroundColor: string;
+    };
+  };
+  [key: string]: any;
+}
+
+
 export default new Vuex.Store({
   state: {
     styles: {
-      card: {
-        backgroundColor: '#fff',
+      currentTheme: 'light',
+      light: {
+        card: {
+          backgroundColor: '#fff',
+        },
       },
-    },
+    } as Styles,
   },
   getters: {
-    card: state => {
-      return state.styles.card;
-    }
+    card: (state) => {
+      return state.styles[state.styles.currentTheme].card;
+    },
   },
   mutations: {
 
