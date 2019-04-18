d4101653b338262827976f6596686cec4d896ef2
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-04-18 10:43:39 -0300

    style

--- src/components/Nav-link.vue
+++ src/components/Nav-link.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class='nav-link'>
     <router-link :to='to'>
       <slot></slot>
     </router-link>
@@ -18,7 +18,7 @@ export default Vue.extend({
 div {
   position: relative;
   display: inline-block;
-  height: 43px;
+  height: 100%;
 }
 
 div:hover {
@@ -27,13 +27,10 @@ div:hover {
 
 a {
   display: inline-block;
-  padding: 12px;
+  padding: 12px 3px;
+  margin: 2px 5px;
   text-decoration: none;
-  transition-duration: .2;
-}
-
-div:hover > a {
-  color: #30e8e2;
+  transition-duration: .1s;
 }
 
 </style>
