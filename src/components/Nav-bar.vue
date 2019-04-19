7a786057c7eff365bf50cb9bd030ad350a078769
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-04-19 20:05:13 -0300

    Revert "Merge pull request #18 from Gustavodacrvi/login-form"
    This reverts commit 3e7e929d712b83f10dd8db7adbcd23b848dc194a, reversing
    changes made to 54ffd88f3ae2e359b5f3e2f9b95fbc778dec4d2c.

--- src/components/Nav-bar.vue
+++ src/components/Nav-bar.vue
@@ -1,10 +1,10 @@
 <template>
-  <nav :class="$store.getters.style('card')">
+  <nav :class="$store.getters.card">
     <section>
     </section>
     <section>
-      <nav-link to='/'>Home</nav-link>
-      <nav-link to='/login'>Login</nav-link>
+      <nav-link to='/home'>Home</nav-link>
+      <nav-link to='/test'>Login</nav-link>
     </section>
     <section>
     </section>
