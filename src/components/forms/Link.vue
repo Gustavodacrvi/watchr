904888e8b79f00fafd3e361746cae234691fcd57
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-04-19 14:00:01 -0300

    form link

---
+++ src/components/forms/Link.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='form-link'>
+    <route :to='to'><slot></slot></route>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+Vue.directive('margin', {
+  bind(el, binding) {
+    const a: any = el.firstChild;
+
+    a.style.marginLeft = binding.value;
+  },
+});
+
+export default Vue.extend({
+  props: {
+    to: String,
+  },
+});
+</script>
+
+<style scoped>
+
+.form-link {
+  margin-bottom: 10px;
+}
+
+</style>
--- src/components/views/Login.vue
+++ src/components/views/Login.vue
@@ -5,7 +5,7 @@
         <heading :lvl='1' v-align>Login</heading>
         <app-input name='username' placeholder='Username or e-mail:' :max='50' type='text'></app-input>
         <app-input name='password' placeholder='Password:' :max='50' type='password'></app-input>
-        <route to='/'>Don't have an account? Sign up!</route>
+        <form-route to='/' v-margin='"5%"'>Don't have an account? Sign up!</form-route>
       </app-form>
     </div>
   </div>
@@ -16,11 +16,13 @@ import Vue from 'vue';
 
 import Form from './../forms/Form.vue';
 import Input from './../forms/Input.vue';
+import Link from './../forms/Link.vue';
 
 export default Vue.extend({
   components: {
     'app-form': Form,
     'app-input': Input,
+    'form-route': Link,
   },
 });
 </script>
@@ -30,6 +32,7 @@ export default Vue.extend({
 div.login {
   display: flex;
   justify-content: center;
+  margin: 4px;
 }
 
 div.login > div {
