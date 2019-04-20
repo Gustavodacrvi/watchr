26025a1fb9965e1e46f17037d65b78d21cecde55
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-04-20 11:47:26 -0300

    ConfirmPassword.vue file

---
+++ src/components/forms/ConfirmPassword.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class='confirm-password'>
+    <app-input :class='inputClass' name='password' :placeholder='placeholder1' :max='max' type='password'></app-input>
+    <app-input :class='inputClass' name='password' :placeholder='placeholder2' :max='max' type='password'></app-input>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Input from './Input.vue';
+
+export default Vue.extend({
+  components: {
+    'app-input': Input,
+  },
+  props: {
+    name: String,
+    max: Number,
+    placeholder1: String,
+    placeholder2: String,
+    inputClass: String,
+  },
+});
+</script>
+
+<style scoped>
+
+div.confirm-password {
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
--- src/components/views/Signup.vue
+++ src/components/views/Signup.vue
@@ -5,6 +5,7 @@
         <app-title class='form-margin'>Sign-up</app-title>
         <app-input class='form-margin' name='username' placeholder='Username:' :max='50' type='text'></app-input>
         <app-input class='form-margin' name='email' placeholder='E-mail:' :max='50' type='text'></app-input>
+        <confirm-password input-class='form-margin' name='password' placeholder1='New password:' placeholder2='Confirm your password:' :max='50'></confirm-password>
         <app-button class='form-margin'>Create account</app-button>
       </app-form>
     </div>
@@ -18,6 +19,7 @@ import Form from './../forms/Form.vue';
 import Input from './../forms/Input.vue';
 import Button from './../forms/Button.vue';
 import Title from './../forms/Title.vue';
+import ConfirmPassword from './../forms/ConfirmPassword.vue';
 
 export default Vue.extend({
   components: {
@@ -25,6 +27,7 @@ export default Vue.extend({
     'app-input': Input,
     'app-button': Button,
     'app-title': Title,
+    'confirm-password': ConfirmPassword,
   },
 });
 </script>
