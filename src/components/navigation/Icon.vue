4b51c0bac1710f4deb3433e067f602166b70ed05
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-05-09 17:40:21 -0300

    mobile hamburger-icon

---
+++ src/components/navigation/Icon.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id='hamburger-icon'>
+    <span class='line'></span>
+    <span class='line'></span>
+    <span class='line'></span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+});
+</script>
+
+
+<style scoped>
+
+#hamburger-icon {
+  display: inline-block;
+  width: 32px !important;
+  height: 28px !important;
+  position: relative;
+  cursor: pointer;
+}
+
+.line {
+  position: absolute;
+  height: 5px;
+  border-radius: 1px;
+  background-color: #ADADAD;
+}
+
+#hamburger-icon > :nth-child(1) {
+  top: 0;
+  width: 100%;
+}
+
+#hamburger-icon > :nth-child(3) {
+  bottom: 0;
+  width: 100%;
+}
+
+#hamburger-icon > :nth-child(2) {
+  width: 22px;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+</style>
+
--- src/components/navigation/Nav-bar.vue
+++ src/components/navigation/Nav-bar.vue
@@ -1,14 +1,17 @@
 <template>
   <desktop v-if='isDesktop'></desktop>
+  <mobile v-else></mobile>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Desktop from './desktop.vue';
+import Desktop from './Desktop.vue';
+import Mobile from './Mobile.vue';
 
 export default Vue.extend({
   components: {
     desktop: Desktop,
+    mobile: Mobile,
   },
   computed: {
     isDesktop() {
