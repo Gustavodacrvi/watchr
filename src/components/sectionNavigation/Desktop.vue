96eae9abf19cbe05a6e4f64bbfb45eb4988f7909
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-05-11 11:48:13 -0300

    navigation icons

--- src/components/sectionNavigation/Desktop.vue
+++ src/components/sectionNavigation/Desktop.vue
@@ -1,24 +1,38 @@
 <template>
   <div id='navigation'>
+    <icon class='pointer icon-color-hover' sz='big' ico='home'></icon>
+    <icon class='pointer icon-color-hover' sz='big' ico='tasks'></icon>
+    <icon class='pointer icon-color-hover' sz='big' ico='calendar'></icon>
+    <icon class='pointer icon-color-hover' sz='big' ico='stopwatch'></icon>
+    <icon class='pointer icon-color-hover' sz='big' ico='stream'></icon>
+    <icon class='pointer icon-color-hover' sz='big' ico='tags'></icon>
+    <icon class='pointer icon-color-hover' sz='big' ico='chart-pie'></icon>
+    <icon class='pointer icon-color-hover' sz='big' ico='settings'></icon>
+    <icon class='pointer icon-color-hover' sz='big' ico='info'></icon>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Icon from './../generalComponents/Icon.vue';
+
 export default Vue.extend({
+  components: {
+    icon: Icon,
+  },
 });
 </script>
 
 <style scoped>
 
 #navigation {
-  background-color: red;
   position: absolute;
   left: 0;
   top: 50%;
   transform: translateY(-50%);
-  width: 100px;
-  height: 100px;
+  width: 87px;
+  display: flex;
+  flex-direction: column;
 }
 
 </style>
