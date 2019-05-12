37d470419c5d4c6f5ec7d98c3342f141e46f5753
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-05-12 12:21:53 -0300

    mobile section

--- src/components/sectionNavigation/Mobile.vue
+++ src/components/sectionNavigation/Mobile.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
-    I am mobile
+  <div id='navigation'>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+
 export default Vue.extend({
+  computed: {
+    lang(): string {
+      return this.$store.getters['lang/l'];
+    },
+  },
 });
 </script>
 
 <style scoped>
+
+#navigation {
+  position: fixed;
+  bottom: 0;
+  height: 50px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
 
 </style>
