09904df3270079ede7bad26485a8aa9815af084c
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-05-19 12:57:36 -0300

    AppNavBar.vue

---
+++ src/components/appNavigation/AppNavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='app-navBar'>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  
+});
+</script>
+
+<style scoped>
+
+.app-navBar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 20px 18px;
+}
+
+</style>
