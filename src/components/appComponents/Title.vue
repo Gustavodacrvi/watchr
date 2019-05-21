100924baac6dd544e206af28354e693ec4a8f172
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-05-21 14:17:05 -0300

    app title component

---
+++ src/components/appComponents/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <span class='app-title'>
+    <app-title :lvl='lvl'><slot></slot></app-title>
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Heading from '@/components/generalComponents/Heading.vue';
+
+export default Vue.extend({
+  components: {
+    'app-title': Heading,
+  },
+  props: {
+    lvl: Number,
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
+
--- src/components/appSections/overview/Today.vue
+++ src/components/appSections/overview/Today.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
-    today
+    <app-title :lvl='1'>Today</app-title>
+    <routine></routine>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Routine from '@/components/appComponents/Routine.vue';
+import Title from '@/components/generalComponents/Heading.vue';
 
 export default Vue.extend({
+  components: {
+    'routine': Routine,
+    'app-title': Title,
+  },
 });
 </script>
 
 <style scoped>
+
+.perspective {
+  margin-top: -25px;
+}
 
 </style>
