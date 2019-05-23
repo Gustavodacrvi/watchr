457619abbc326dc39ce9b2d032587d90d0eafe8f
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-05-23 18:27:18 -0300

    icons

--- src/components/appSections/overview/Inbox.vue
+++ src/components/appSections/overview/Inbox.vue
@@ -1,21 +1,40 @@
 <template>
   <div>
-    <app-title :lvl='1'>Inbox</app-title>
+    <app-title class='' :lvl='1'>Inbox</app-title>
+    <div>
+      <icon class='right icon-group-icon pointer circle' ico='circle' sz='medium' ></icon>
+      <icon class='right icon-group-icon pointer' ico='filter' sz='medium' ></icon>
+      <icon class='left icon-group-icon pointer' ico='search' sz='medium' ></icon>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Title from '@/components/appComponents/Title.vue';
-import IconGroup from '@/components/dropdown/IconGroup.vue';
+import Icon from '@/components/generalComponents/Icon.vue';
 
 export default Vue.extend({
   components: {
     'app-title': Title,
+    'icon': Icon,
   },
 });
 </script>
 
 <style scoped>
+
+
+.icon {
+  margin: 2px;
+}
+
+.right {
+  float: right;
+}
+
+.left {
+  float: left;
+}
 
 </style>
