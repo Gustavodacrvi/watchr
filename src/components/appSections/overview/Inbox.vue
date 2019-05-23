5c195a4d44a1f427ddf5ab0cc127b4a444cf9464
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-05-23 18:00:18 -0300

    inbox title

--- src/components/appSections/overview/Inbox.vue
+++ src/components/appSections/overview/Inbox.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    inbox
+    <app-title :lvl='1'>Inbox</app-title>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Title from '@/components/appComponents/Title.vue';
+import IconGroup from '@/components/dropdown/IconGroup.vue';
 
 export default Vue.extend({
+  components: {
+    'app-title': Title,
+  },
 });
 </script>
 
--- src/store_modules/app/app.ts
+++ src/store_modules/app/app.ts
@@ -6,7 +6,7 @@ const sameDay = (d1: Date, d2: Date) => {
   return d1.getFullYear() === d2.getFullYear() &&
     d1.getMonth() === d2.getMonth() &&
     d1.getDate() === d2.getDate();
-}
+};
 
 export default {
   namespaced: true,
@@ -68,7 +68,6 @@ export default {
         if (data !== null) {
           state.interval = JSON.parse(data);
         }
-        console.log(state)
       }
     },
     saveRoutines(state: any) {
