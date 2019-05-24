9b475191b7785cdffa950c92d41667e7c4a27143
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-05-24 10:04:07 -0300

    section input.vue

--- src/components/appNavigation/SectionInput.vue
+++ src/components/appNavigation/SectionInput.vue
@@ -1,15 +1,23 @@
 <template>
   <transition name='fade-transition'>
-    <div class='section-input' v-if='show'>
-      <input :class='$store.state.theme.style' class='input' name='input' :placeholder='placeholder' autocomplete='off' v-model='value'/>      
+    <div class='section-input' v-if='showing'>
+      <div>
+        <input :class='$store.state.theme.style' class='input' name='input' :placeholder='placeholder' autocomplete='off' v-model='value'/>      
+      </div>
+      <alert class='left pointer' @click='$emit("click")'>Add</alert>
+      <alert class='right pointer' type='error' @click='showing = false'>Cancel</alert>
     </div>
   </transition>  
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Alert from '@/components/generalComponents/Alert.vue';
 
 export default Vue.extend({
+  components: {
+    alert: Alert,
+  },
   props: {
     placeholder: String,
     show: Boolean,
@@ -17,6 +25,7 @@ export default Vue.extend({
   data() {
     return {
       value: '',
+      showing: this.show,
     };
   },
 });
@@ -24,5 +33,26 @@ export default Vue.extend({
 
 <style scoped>
 
-</style>
+.section-input {
+  margin: 20px 0;
+}
 
+.input {
+  background-color: transparent;
+  outline: none;
+  padding: 6px;
+  color: #999999;
+  font-family: 'Work Sans';
+  border-radius: 12px;
+  border: 1px solid #999999;
+}
+
+.left {
+  float: left;
+}
+
+.right {
+  float: right;
+}
+
+</style>
