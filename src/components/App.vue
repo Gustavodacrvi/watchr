536981482827c57c6b825913ed9ec063ea369764
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-06-16 16:30:34 -0300

    app.vue

---
+++ src/components/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <span>
+    app
+  </span>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import { Mutation, Getter } from 'vuex-class'
+
+
+@Component
+export default class Guest extends Vue {
+  @Prop({required: true}) public readonly localStorage!: boolean
+  @Getter('isDesktop') public readonly isDesktop!: boolean
+  @Mutation('openAppBar') public readonly openAppBar!: () => void
+
+  public created() {
+    if (this.isDesktop) {
+      this.openAppBar()
+    }
+  }
+}
+
+</script>
+
--- src/views/Guest.vue
+++ src/views/Guest.vue
@@ -1,23 +1,20 @@
 <template>
   <div>
-    {{ $store.state.appBarState }}
+    <app :localStorage='true'></app>
   </div>
 </template>
 
 <script lang='ts'>
 
 import { Component, Vue } from 'vue-property-decorator'
-import { Mutation, Getter } from 'vuex-class'
 
-@Component
-export default class Guest extends Vue {
-  @Getter('isDesktop') public readonly isDesktop!: boolean
-  @Mutation('openAppBar') public readonly openAppBar!: () => void
-  public created() {
-    if (this.isDesktop) {
-      this.openAppBar()
-    }
-  }
-}
+import App from '@/components/App.vue'
+
+@Component({
+  components: {
+    app: App,
+  },
+})
+export default class Guest extends Vue {}
 
 </script>
