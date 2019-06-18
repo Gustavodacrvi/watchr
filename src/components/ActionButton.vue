924c456ff1534c763da7284dfb5322203df6848a
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-06-18 15:58:36 -0300

    action buttons

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,9 +1,14 @@
 <template>
   <div class='wrapper'>
     <div class='relative-wrapper'>
-      <span class='main' :class='{close: !showing}' @click='showing = !showing'>
+      <span class='main' :class='{close: showing}' @click='showing = !showing'>
         <icon class='icon' icon='plus' color='white'></icon>
       </span>
+      <div class='left-wrapper'>
+        <span class='btn left' v-for='btn in leftButtons' :key='btn.icon' :style='`background-color: ${btn.backColor}`' @click='btn.click'>
+          <icon :icon='btn.icon' :color='btn.iconColor'></icon>
+        </span>
+      </div>
     </div>
   </div>
 </template>
@@ -13,12 +18,25 @@
 import { Component, Vue } from 'vue-property-decorator'
 import FontAwesomeIcon from '@/components/FontAwesomeIcon.vue'
 
+interface Buttons {
+  icon: string
+  iconColor: string
+  backColor: string
+  click: () => void
+}
+
 @Component({
   components: {
     icon: FontAwesomeIcon,
   },
 })
 export default class ActionButton extends Vue {
+  public leftButtons: Buttons[] = [
+    {icon: 'inbox', iconColor: 'white', backColor: '#83B7E2', click: () => console.log(3)},
+    {icon: 'calendar-day', iconColor: 'white', backColor: '#FFE366', click: () => console.log(3)},
+    {icon: 'calendar-alt', iconColor: 'white', backColor: '#FF6B66', click: () => console.log(3)},
+  ]
+
   public showing: boolean = false
 }
 
@@ -56,9 +74,32 @@ export default class ActionButton extends Vue {
   transition-duration: .3s;
 }
 
-
 .main.close {
   transform: rotate(45deg);
+}
+
+.left-wrapper {
+  position: absolute;
+  height: 45px;
+  bottom: 16px;
+  right: 70px;
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+}
+
+.btn {
+  display: inline-block;
+  border-radius: 100px;
+  width: 40px;
+  height: 40px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn.left {
+  margin: 0 6px;
 }
 
 </style>
