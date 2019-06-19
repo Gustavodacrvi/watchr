4c6efad11e405c7c43b032bfa8ad6ae2ca2def4c
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-06-19 16:57:30 -0300

    addLabelFromArrayPath mutation

--- src/components/PopUps/LabeladderPopup.vue
+++ src/components/PopUps/LabeladderPopup.vue
@@ -5,7 +5,7 @@
     </div>
     <div class='content'>
       <input tabindex='1' class='input gray round-border txt margin' type='text' autocomplete='off' :class='theme' v-model='value' @keypress='keyPressed' />
-      <button tabindex='2' class='button round-border margin' @click='addLabel'>Add label</button>
+      <button tabindex='2' class='button round-border margin' @click='add'>Add label</button>
     </div>
   </div>
 </template>
@@ -13,7 +13,7 @@
 <script lang='ts'>
 
 import { Component, Vue } from 'vue-property-decorator'
-import { State, Getter, Mutation, namespace } from 'vuex-class'
+import { State, Getter, Mutation, Action, namespace } from 'vuex-class'
 
 import labelUtil from '@/utils/label'
 
@@ -27,22 +27,32 @@ export default class LabelAdder extends Vue {
   @State('theme') public readonly theme!: string
   @Mutation('pushAlert') public readonly pushAlert!: (alert: Alert) => void
   // tslint:disable-next-line:max-line-length
+  @labelModule.Mutation('addLabelFromArrayPath') public readonly addLabelFromArrayPath!: (path: string[]) => void
+  // tslint:disable-next-line:max-line-length
   @labelModule.Getter('getLabelNodeFromArrayPath') public readonly  getLabelNodeFromArrayPath!: (path: string[]) => Label | undefined
 
   public value: string = ''
 
   public keyPressed({key}: {key: string}): void {
     if (key === 'Enter') {
-      this.addLabel()
+      this.add()
     }
   }
-  public addLabel(): void {
-    const label: Label | undefined = this.getLabelNodeFromArrayPath(labelUtil.getArrFromStringPath(this.value))
+  public add(): void {
+    const arr = labelUtil.getArrFromStringPath(this.value)
+    const label: Label | undefined = this.getLabelNodeFromArrayPath(arr)
     if (label !== undefined) {
       this.pushAlert({
         name: `<strong>${this.value}</strong> already exists.`,
         duration: 2.5,
         type: 'error',
+      })
+    } else {
+      this.addLabelFromArrayPath(arr)
+      this.pushAlert({
+        name: `<strong>${this.value}</strong> was successfully added`,
+        duration: 2.5,
+        type: 'success',
       })
     }
   }
