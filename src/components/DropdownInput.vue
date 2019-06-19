f411816441cd61c79c00ad941b9d0080ecc8d907
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-06-19 19:31:41 -0300

    dropdown styles

--- src/components/DropdownInput.vue
+++ src/components/DropdownInput.vue
@@ -1,23 +1,27 @@
 <template>
   <div class='dropdown-input'>
-    <input v-if='tabindex' :tabindex='tabindex' class='margin input txt round-border gray' :placeholder='placeholder' type='text' autocomplete='off' :class='[theme, {wrong: hasError}]' v-model='value'>
-    <input v-else :tabindex='tabindex' class='margin input txt round-border gray' :placeholder='placeholder' type='text' autocomplete='off' :class='[theme, {wrong: hasError}]' v-model='value'>
+    <input v-if='tabindex' :tabindex='tabindex' class='margin input txt round-border gray' :placeholder='placeholder' type='text' autocomplete='off' :class='[theme, {wrong: hasError}]' v-model='value' @keypress='keyPressed'>
+    <input v-else :tabindex='tabindex' class='margin input txt round-border gray' :placeholder='placeholder' type='text' autocomplete='off' :class='[theme, {wrong: hasError}]' v-model='value' @keypress='keyPressed'>
+    <div class='dropdown round-border gray border' :class='theme'>
+      <span class='option txt' :class='[theme,{active: option === selected}]' v-for='option in values' :key='option'>{{ option }}</span>
+    </div>
   </div>
 </template>
 
 <script lang='ts'>
 
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
 import { State } from 'vuex-class'
 
 @Component
 export default class DropdownInput extends Vue {
+  @State('theme') public readonly theme!: string
+  @Prop({default: null, type: String}) public readonly input!: string
   @Prop({default: () => [], type: Array}) public readonly values!: string[]
   @Prop({type: String}) public readonly tabindex!: string[]
   @Prop({type: String}) public readonly placeholder!: string[]
-  @State('theme') public readonly theme!: string
 
-  public value: string | null = null
+  public value: string | null = this.input
   public selected: string = ''
 
   get hasError(): boolean {
@@ -29,9 +33,54 @@ export default class DropdownInput extends Vue {
       this.$emit('enter')
     }
   }
+
+  @Watch('input')
+  onInput(): void {
+    this.value = this.input
+  }
+  @Watch('value')
+  onValue(): void {
+    this.$emit('value', this.value)
+  }
 }
 
 </script>
 
 <style scoped src='@/assets/css/authPopUp.css'>
 </style>
+
+<style scoped>
+
+.dropdown-input {
+  position: relative;
+}
+
+.dropdown {
+  position: absolute;
+  top: 100%;
+  width: 100%;
+  max-height: 300px;
+  overflow: hidden;
+}
+
+.option {
+  display: block;
+  padding: 10px;
+  cursor: pointer;
+  transition: background-color .3s;
+}
+
+.option:hover, .option.active {
+  background-color: #292929;
+}
+
+.option.dark + .option.dark {
+  border-top: .5px solid #292929;
+}
+
+.option.light + .option.light {
+  border-top: .5px solid #D9D9D9;
+}
+
+</style>
+
--- src/components/PopUps/LabeladderPopup.vue
+++ src/components/PopUps/LabeladderPopup.vue
@@ -4,7 +4,7 @@
       <h3>Add label</h3>
     </div>
     <div class='content'>
-      <dropdown-input tabindex='1' class='margin' @value='v => value = v' @enter='add'></dropdown-input>
+      <dropdown-input tabindex='1' class='margin' :values='options' @value='v => value = v' @enter='add'></dropdown-input>
       <button tabindex='2' class='button round-border margin' @click='add'>Add label</button>
       <span v-show='isDesktop' class='margin txt'>You can open this pop up at any time by clicking the 'L' key.</span><br>
       <span v-show='isDesktop' class='margin txt'>You can close any pop up at any time by clicking 'H' key.</span>
@@ -14,7 +14,7 @@
 
 <script lang='ts'>
 
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Watch } from 'vue-property-decorator'
 import { State, Getter, Mutation, Action, namespace } from 'vuex-class'
 
 import DropdownInput from '@/components/DropdownInput.vue'
@@ -36,11 +36,13 @@ export default class LabelAdder extends Vue {
   @Getter('isDesktop') public readonly isDesktop!: boolean
   @Mutation('pushAlert') public readonly pushAlert!: (alert: Alert) => void
   // tslint:disable-next-line:max-line-length
+  @labelModule.State('labels') public readonly labels!: Label[]
   @labelModule.Mutation('addLabelFromArrayPath') public readonly addLabelFromArrayPath!: (path: string[]) => void
   // tslint:disable-next-line:max-line-length
   @labelModule.Getter('getLabelNodeFromArrayPath') public readonly  getLabelNodeFromArrayPath!: (path: string[]) => Label | undefined
 
   public value: string = ''
+  public options: string[] = []
 
   public add(): void {
     const arr = labelUtil.getArrFromStringPath(this.value)
@@ -64,6 +66,21 @@ export default class LabelAdder extends Vue {
         duration: 2.5,
         type: 'success',
       })
+    }
+  }
+
+
+  @Watch('value')
+  onValueChange(): void {
+    const arr = labelUtil.getArrFromStringPath(this.value)
+    if (arr.length > 1) { 
+      arr.pop()
+      const label: Label | undefined = this.getLabelNodeFromArrayPath(arr)
+      if (label !== undefined) {
+        this.options = label.subLabels.map((el: Label) => el.name)
+      }
+    } else {
+      this.options = this.labels.map((el: Label) => el.name)
     }
   }
 }
