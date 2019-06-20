c4ecb70875adca1719b9a66dcbcc668d22b226a4
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-06-20 10:29:43 -0300

    move scroll when on hided selected

--- src/components/DropdownInput.vue
+++ src/components/DropdownInput.vue
@@ -1,11 +1,11 @@
 <template>
   <div class='dropdown-input'>
-    <input v-if='tabindex' :tabindex='tabindex' class='margin input txt round-border gray' :placeholder='placeholder' type='text' autocomplete='off' :class='[theme, {wrong: hasError}]' v-model='value' @keydown='keyDown' @keypress='keyPressed' @focus='showing = true' @blur='showing = false'>
-    <input v-else :tabindex='tabindex' class='margin input txt round-border gray' :placeholder='placeholder' type='text' autocomplete='off' :class='[theme, {wrong: hasError}]' v-model='value' @keydown='keyDown' @keypress='keyPressed'  @focus='showing = true' @blur='showing = false'>
+    <input v-if='tabindex' :tabindex='tabindex' class='margin input txt round-border gray' :placeholder='placeholder' type='text' autocomplete='off' :class='[theme, {wrong: hasError}]' v-model='value' @keydown='keyDown' @keypress='keyPressed' @focus='focus' @blur='blur'>
+    <input v-else :tabindex='tabindex' class='margin input txt round-border gray' :placeholder='placeholder' type='text' autocomplete='off' :class='[theme, {wrong: hasError}]' v-model='value' @keydown='keyDown' @keypress='keyPressed'  @focus='focus' @blur='blur'>
     <transition name='fade'>
-      <div v-if='showing' class='dropdown round-border gray border' :class='theme'>
+      <div v-if='showing' class='dropdown round-border gray border' :class='theme' ref='dropdown'>
         <transition-group name='fade'>
-          <span class='option txt' :class='[theme,{active: option === selected}]' v-for='option in values' :key='option' @click='select(option)'>{{ option }}</span>
+          <span v-for='option in values' :ref='option' class='option txt' :class='[theme,{active: selected === option}]' :key='option' @click='select(option)'>{{ option }}</span>
         </transition-group>
       </div>
     </transition>
@@ -16,6 +16,17 @@
 
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
 import { State } from 'vuex-class'
+
+interface RefsPositions {
+  drop: {
+    height: number
+    scroll: number,
+  }
+  act: {
+    top: number
+    height: number,
+  }
+}
 
 @Component
 export default class DropdownInput extends Vue {
@@ -42,34 +53,72 @@ export default class DropdownInput extends Vue {
     this.$emit('update')
   }
   public keyDown({key}: {key: string}): void {
-    this.moveSelection(key)
+    if (key === 'ArrowDown' || key === 'ArrowUp') {
+      this.moveSelection(key)
+    } else if (key === 'ArrowLeft' || key === 'ArrowRight') {
+      this.selected = ''
+    }
+  }
+  public getRefsPositions(ref: string): RefsPositions {
+    /* tslint:disable:no-string-literal */
+    const drop: any = this.$refs['dropdown']
+    let active: any = this.$refs[ref]
+    active = active[0]
+    const activeHeight: number = active.clientHeight
+    const activeTop: number = active.offsetTop
+    const dropHeight: number = drop.clientHeight
+    const scroll: number = drop.scrollTop
+
+    return {
+      act: {
+        height: activeHeight,
+        top: activeTop,
+      },
+      drop: {
+        height: dropHeight,
+        scroll,
+      },
+    }
   }
   public moveSelection(key: string): void {
-    const index = this.values.findIndex((el: string) => {
-      return el === this.selected
-    })
-    if (key === 'ArrowDown') {
-      if (this.selected === '') {
-        this.selected = this.values[0]
-      } else {
-        if (index + 1 < this.values.length) {
-          this.selected = this.values[index + 1]
-        } else {
-          this.selected = ''
+    if (this.selected === '') {
+      this.selected = this.values[0]
+    } else {
+      const index = this.values.findIndex((el: string) => {
+        return el === this.selected
+      })
+      /* tslint:disable:no-string-literal */
+      const drop: any = this.$refs['dropdown']
+
+      if (key === 'ArrowDown' && index + 1 < this.values.length) {
+        this.selected = this.values[index + 1]
+        const p = this.getRefsPositions(this.selected)
+
+        if (p.act.top + p.act.height > p.drop.height + p.drop.scroll) {
+          drop.scrollTop += p.act.height
         }
-      }
-    } else if (key === 'ArrowUp') {
-      if (this.selected === this.values[0]) {
+      } else if (key === 'ArrowUp' && index !== 0) {
+        this.selected = this.values[index - 1]
+        const p = this.getRefsPositions(this.selected)
+
+        if (p.act.top < p.drop.scroll) {
+          drop.scrollTop -= p.act.height
+        }
+      } else if (index + 1 !== this.values.length) {
         this.selected = ''
-      } else {
-        if (index !== 0) {
-          this.selected = this.values[index - 1]
-        }
       }
     }
   }
   public select(str: string): void {
     this.$emit('select', str)
+    this.selected = ''
+  }
+  public blur(): void {
+    this.showing = false
+    this.selected = ''
+  }
+  public focus(): void {
+    this.showing = true
     this.selected = ''
   }
 
@@ -79,6 +128,7 @@ export default class DropdownInput extends Vue {
   }
   @Watch('value')
   public onValue(): void {
+    this.selected = ''
     this.$emit('value', this.value)
     this.$emit('update')
   }
@@ -100,7 +150,7 @@ export default class DropdownInput extends Vue {
   top: 100%;
   width: 100%;
   max-height: 250px;
-  overflow: hidden;
+  overflow: auto;
 }
 
 .option {
