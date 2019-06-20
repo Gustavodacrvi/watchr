fde5de6b8d46d963280f9c9298a89606f70a4291
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-06-20 10:55:32 -0300

    LabelAppnav.vue

---
+++ src/components/TheAppBar/AppnavSections/LabelAppnav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    label
+  </div>
+</template>
+
+<script lang='ts'>
+
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class OverviewAppnav extends Vue {}
+
+</script>
+
+<style scoped>
+
+</style>
