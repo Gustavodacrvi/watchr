9876bc92928cc9ffdc0d787d1e7790d4906031a3
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-06-26 14:30:46 -0300

    firebase auth setup

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,21 @@ import { FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser, faAdjust, faSignInAlt, faUserAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 import { dom } from '@fortawesome/fontawesome-svg-core'
+import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+}
+
+firebase.initializeApp(config)
 
 library.add(faUser, faAdjust, faUserAlt, faSignInAlt, faUserPlus)
 
