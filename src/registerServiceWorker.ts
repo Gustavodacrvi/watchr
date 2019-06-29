7ecc48d2958d5fe47e36516145646bda876102df
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-06-28 21:03:40 -0300

    final

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -2,7 +2,7 @@
 
 import { register } from 'register-service-worker'
 
-console.log(`${process.env.BASE_URL}service-worker.js`)
+// process.env.NODE_ENV === 'production'
 
 if (true)
   register(`${process.env.BASE_URL}service-worker.js`, {
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   pwa: {
     themeColor: '#fc7d7d',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       swSrc: 'public/service-worker.js',
