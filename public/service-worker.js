e7fb18d554a5d0eed5a9526861802a51efdb691b
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-06-29 17:19:37 -0300

    cache versioning

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,16 @@
 const cacheName = 'cache'
 
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      )
+    })
+  )
+})
+
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
