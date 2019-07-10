23f34055911b331c9528c41619a296fe4bf2a669
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-07-10 20:57:19 -0300

    hide filters toggle

--- src/components/AppViews/AppviewComponents/AppviewTags.vue
+++ src/components/AppViews/AppviewComponents/AppviewTags.vue
@@ -1,7 +1,13 @@
 <template>
   <div class='tags-wrapper'>
+    <dynamic-ft-icon
+      class='toggle-icon pointer icon'
+      icon='bars'
+      size='lg'
+      @click='showing = !showing'
+    />
     <transition name='fade'>
-      <div class='tags'>
+      <div v-if='showing' class='tags'>
         <view-tag v-if='fixedTag'
           :name='fixedTag'
           :fixed='true'
@@ -33,23 +39,32 @@ import { Component, Vue, Prop } from 'vue-property-decorator'
 import { State } from 'vuex-class'
 
 import Tag from '@/components/AppViews/AppviewComponents/AppviewTag.vue'
+import FontAwesome from '@/components/DynamicFontawesome.vue'
 
 @Component({
   components: {
     'view-tag': Tag,
+    'dynamic-ft-icon': FontAwesome,
   },
 })
 export default class AppviewTags extends Vue {
   @Prop(String) fixedTag!: string
   @Prop(String) search!: string
   @Prop(String) priority!: string
+
+  showing: boolean = true
 }
 
 </script>
 
 <style scoped>
 
-.tags-wrapper {
+.toggle-icon {
+  float: right;
+  margin-bottom: 5px;
+}
+
+.tags {
   margin-bottom: 25px;
 }
 
