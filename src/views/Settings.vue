230bc91ca9abaed06f04b6bee74d6a890db79c4a
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-07-11 16:11:13 -0300

    desktop links

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,37 +1,48 @@
 <template>
-  <div class='settings'>
+  <div class='settings background-color' :class='theme'>
     <div class='center'>
-      <div class='menu'>
-        <router-link
-          class='txt el'
-          ref='About'
-          :to="{name: 'About'}"
-        >About</router-link>
-        <router-link
-          class='txt el'
-          ref='General'
-          :to="{name: 'General'}"
-        >General</router-link>
-        <router-link
-          class='txt el'
-          ref='Privacy policy'
-          :to="{name: 'Privacy policy'}"
-        >Privacy policy</router-link>
-        <router-link
-          class='txt el'
-          ref='Security policy'
-          :to="{name: 'Security policy'}"
-        >Security policy</router-link>
-        <router-link
-          class='txt el'
-          ref='Terms of service'
-          :to="{name: 'Terms of service'}"
-        >Terms of service</router-link>
+      <div class='fake-menu'></div>
+      <div class='menu-wrapper' :class='theme'>
+        <div class='menu'>
+          <router-link
+            class='txt el'
+            ref='About'
+            :to="{name: 'About'}"
+          >About</router-link>
+          <router-link
+            class='txt el'
+            ref='General'
+            :to="{name: 'General'}"
+          >General</router-link>
+          <div class='margin'></div>
+          <router-link
+            class='txt el'
+            ref='Privacy policy'
+            :to="{name: 'Privacy policy'}"
+          >Privacy policy</router-link>
+          <router-link
+            class='txt el'
+            ref='Security policy'
+            :to="{name: 'Security policy'}"
+          >Security policy</router-link>
+          <router-link
+            class='txt el'
+            ref='Terms of service'
+            :to="{name: 'Terms of service'}"
+          >Terms of service</router-link>
+        </div>
       </div>
       <div class='text txt'>
         <transition name='fade' mode='out-in'>
           <router-view/>
         </transition>
+        <div class='margin'></div>
+        <div class='margin'></div>
+        <div class='margin'></div>
+        <div class='margin'></div>
+        <div class='margin'></div>
+        <div class='margin'></div>
+        <div class='margin'></div>
       </div>
     </div>
   </div>
@@ -40,9 +51,12 @@
 <script lang='ts'>
 
 import { Component, Vue } from 'vue-property-decorator'
+import { State } from 'vuex-class'
 
 @Component
-export default class Help extends Vue {}
+export default class Help extends Vue {
+  @State theme!: string
+}
 
 </script>
 
@@ -51,7 +65,6 @@ export default class Help extends Vue {}
 .settings {
   position: relative;
   width: 100%;
-  min-height: 100%;
   display: flex;
   justify-content: center;
 }
@@ -61,16 +74,56 @@ export default class Help extends Vue {}
   margin: 0 10px;
   margin-top: 60px;
   display: flex;
-}
-
-.menu {
-  flex-basis: 225px;
+  align-items: flex-start;
 }
 
 .text {
   flex-basis: 775px;
   text-align: justify;
   text-justify: inter-word;
+}
+
+.menu-wrapper {
+  position: fixed;
+  flex-basis: 225px;
+}
+
+.fake-menu {
+  flex-basis: 225px;
+}
+
+.menu-wrapper.dark {
+  border-left: 2px solid #292929;
+}
+
+.menu-wrapper.light {
+  border-left: 2px solid #D9D9D9;
+}
+
+.menu {
+  position: relative;
+  left: -1px;
+}
+
+.margin {
+  height: 25px;
+}
+
+.el {
+  display: block;
+  padding: 6px;
+  padding-left: 12px;
+  text-decoration: none;
+  font-size: 1.1em;
+  transition: color .3s, border .3s, padding .3s;
+}
+
+.el:hover, .el.router-link-exact-active {
+  color: #fc7d7d;
+  border-left: 2px solid #fc7d7d;
+  position: relative;
+  left: -1px;
+  padding-left: 13px;
 }
 
 </style>
