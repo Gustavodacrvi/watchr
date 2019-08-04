0e5dab985f0cf3377853c60b2fe06f64086e360a
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-08-04 12:15:26 -0300

    search icon overflowing screen

--- src/components/AppViews/AppviewComponents/AppviewHeadericons.vue
+++ src/components/AppViews/AppviewComponents/AppviewHeadericons.vue
@@ -29,7 +29,7 @@
         handle='search'
         :class='theme'
         :size='size'
-        min-width='300px'
+        min-width='250px'
         title='Search tasks'
         v-model='search'
       />
