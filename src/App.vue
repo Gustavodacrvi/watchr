4fd1899f50077447f92174156fca6b5928d8f7fc
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-09-17 20:26:35 -0300

    font work sans

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <NavBar/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+<script>
+import NavBarVue from './components/NavBar/NavBar.vue';
+
+export default {
+  components: {
+    NavBar: NavBarVue,
+  }  
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style src="@/assets/css/index.css">
 </style>
---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="NavBar">
+    <span>navbar</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+
+</script>
+
+<style scoped>
+
+.NavBar {
+  height: 50px;
+}
+
+</style>
