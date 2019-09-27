98047869b0e11da20455db7645feb5c9f18c5cd7
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-09-27 11:20:03 -0300

    filter tasks by view Tomorrow

--- src/utils/task.js
+++ src/utils/task.js
@@ -38,19 +38,49 @@ export default {
     // TODO
   },
   filterTasksByView(tasks, view) {
-    const taskData = (t) => {
+    const taskData = (t, tod) => {
       const c = t.calendar
 
       return {
+        tod,
         type: c.type,
         defer: mom(c.defer, 'Y-M-D'),
         due: mom(c.due, 'Y-M-D'),
         spec: mom(c.specific, 'Y-M-D'),
-        tod: mom(),
         edit: mom(c.editDate, 'Y-M-D'),
         interval: c.periodic.interval,
         weekDays: c.weekly,
       }
+    }
+    const filterTasksByDay = (dayMoment) => {
+      return tasks.filter(el => {
+        if (!el.calendar) return false
+        const {
+          type, defer, due, tod,
+          edit, spec, interval,
+          weekDays,
+        } = taskData(el, dayMoment)
+        const isOverdue = (due.isBefore(tod, 'day'))
+        const isntReadyYet = (defer.isAfter(tod, 'day'))
+        const notToday = (!tod.isSame(spec, 'day'))
+
+        if (isOverdue) return false
+        if (isntReadyYet) return false
+        
+        if (type === 'specific' && notToday) return false
+        if (type === 'periodic') {
+          const dayDiff = tod.diff(edit, 'day')
+          const eventNotToday = dayDiff % interval !== 0
+          if (eventNotToday) return false
+        }
+        if (type === 'weekly') {
+          const todaysWeekDayName = tod.format('ddd').toLowerCase()
+          const eventNotToday = !weekDays.find(w => w.toLowerCase() === todaysWeekDayName)
+          if (eventNotToday) return false
+        }
+
+        return true
+      })
     }
 
     switch (view) {
@@ -59,34 +89,10 @@ export default {
         // return tasks.filter(el => !el.calendar && !el.list && el.tags.length === 0)
       }
       case 'Today': {
-        return tasks.filter(el => {
-          if (!el.calendar) return false
-          const {
-            type, defer, due, tod,
-            edit, spec, interval,
-            weekDays,
-          } = taskData(el)
-          const isOverdue = (due.isBefore(tod, 'day'))
-          const isntReadyYet = (defer.isAfter(tod, 'day'))
-          const notToday = (!tod.isSame(spec, 'day'))
-
-          if (isOverdue) return false
-          if (isntReadyYet) return false
-          
-          if (type === 'specific' && notToday) return false
-          if (type === 'periodic') {
-            const dayDiff = tod.diff(edit, 'day')
-            const eventNotToday = dayDiff % interval !== 0
-            if (eventNotToday) return false
-          }
-          if (type === 'weekly') {
-            const todaysWeekDayName = tod.format('ddd').toLowerCase()
-            const eventNotToday = !weekDays.find(w => w.toLowerCase() === todaysWeekDayName)
-            if (eventNotToday) return false
-          }
-
-          return true
-        })
+        return filterTasksByDay(mom())
+      }
+      case 'Tomorrow': {
+        return filterTasksByDay(mom().add(1, 'day'))
       }
     }
     
