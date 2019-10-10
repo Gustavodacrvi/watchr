85f5c4c98bf684c140057a72a727eca05119136d
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-10-10 14:22:40 -0300

    search drop option

--- src/components/Popup/FastSearch.vue
+++ src/components/Popup/FastSearch.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="FastSearch" @click="$store.commit('closeFastSearch')">
-    <div class="card">
-      asçdlkfj
+    <div class="cb rb card shadow" @click.stop>
+      <div class="cont">
+        <InputApp :focus='true' v-model="search"/>
+        <div class="options">
+          <div class="option rb cursor">
+            <div class="icon-wrapper">
+              <Icon icon='user' class="icon"/>
+            </div>
+            <span class="name">I am a freaking option motherfucker.</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 
+import InputVue from '@/components/Auth/Input.vue'
+import IconVue from '@/components/Icon.vue'
+
 export default {
-  
+  components: {
+    InputApp: InputVue,
+    Icon: IconVue,
+  },
+  data() {
+    return {
+      search: ''
+    }
+  }
 }
 
 </script>
@@ -20,8 +41,57 @@ export default {
   position: fixed;
   width: 100%;
   height: 100%;
-  background-color: rgba(0,0,0,.4);
   z-index: 499;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+}
+
+.card {
+  flex-basis: 600px;
+  margin: 0 15px;
+  margin-top: 60px;
+  max-height: 400px;
+}
+
+
+.cont {
+  margin: 10px;
+}
+
+.options {
+  margin-top: 20px;
+}
+
+.option {
+  height: 35px;
+  width: 100%;
+  display: flex;
+  background-color: none;
+  transition-duration: .2s;
+}
+
+.option:hover, .active {
+  background-color: var(--light-gray);
+}
+
+.icon-wrapper {
+  flex-basis: 35px;
+  height: 100%;
+  position: relative;
+}
+
+.icon {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+
+.name {
+  display: flex;
+  align-items: center;
+  flex-basis: 100%;
 }
 
 </style>
