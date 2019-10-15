cb9b9f69a8ec8dee30a3e7a0af57e2656f8decb9
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-10-15 20:10:42 -0300

    update message

--- src/components/Popup/Update.vue
+++ src/components/Popup/Update.vue
@@ -4,23 +4,27 @@
       <h2 class="pc">{{ l['New update!'] }}</h2>
     </div>
     <div class="content">
-      <h3>Bug fixes</h3>
+      <h3>Features for Desktop and Mobile</h3>
+      <h2>General</h2>
       <ul>
-        <li>Add task with shortcuts not working properly.</li>
+        <li>Sign in with google.</li>
+        <li>Sign in as a guest.</li>
+        <li>Upgrade guest user with google.</li>
+        <li>Upgrade guest user with email and password.</li>
       </ul>
-      <h3>Features</h3>
-      <h4>Desktop and Mobile</h4>
+      <h2>Date</h2>
       <ul>
-        <li>Convert heading to list option.</li>
-        <li>Add list/tag notes and edit them directly by clicking on the them.</li>
-        <li>Duplicate heading and its tasks option.</li>
-        <li>Duplicate list with its tasks and headings.</li>
-      </ul>
-      <h4>Desktop</h4>
-      <ul>
-        <li>Edit list name and tag name directly by clicking on the header text.</li>
-        <li>Add heading at the end of the list with the key 'h' and at the beginning with the key 'H'.</li>
-        <li>Close task/heading adder with the key 'Esc'.</li>
+        <li>You can now set the number of times a task should be checked before being considered completed. <br><br>
+          Ex: "example task $10 times", will require a check 10 times.<br><br>
+        </li>
+        <li>The same works for periodic/weekly tasks:<br><br>
+          Ex: "Work out for 30min $every 2 days 10 times".<br><br>
+        </li>
+        <li>Period/weekly tasks by default are expected to be completed once a day, that means, once you check the "Work out" task, it will disappear from the Today list, however, sometimes you may want to be able to complete task in advance in the same day, in those cases, you can use the "persistent" or "per" keyword. <br><br>
+
+          Ex: "Read 10 pages of 'O Cortiço' $every day 7 times persistent", the task won't disapper from the Today list until it reaches zero.
+        
+        </li>
       </ul>
       <Button @click="close" value="Got it! Refresh"/>
     </div>
