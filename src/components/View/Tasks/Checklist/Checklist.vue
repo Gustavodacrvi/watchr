855665ee9d7c827523fb1a1d4624e9b363574387
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-10-21 12:56:13 -0300

    Add task vue

--- src/components/View/Tasks/Checklist/Checklist.vue
+++ src/components/View/Tasks/Checklist/Checklist.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="Checklist" :class="{hasAtLeastOnSubTask}">
+    <SubtaskEdit v-if="test"/>
     <transition-group name="trans" appear
       @enter='enter'
       @leave='leave'
@@ -16,14 +17,17 @@
 <script>
 
 import Subtask from './Subtask.vue'
+import EditVue from './Edit.vue'
 
 export default {
   props: ['toggleChecklist', 'order', 'list'],
   components: {
     Subtask,
+    SubtaskEdit: EditVue,
   },
   data() {
     return {
+      test: false,
       render: [
         {
           name: 'I am a subtask mother fucker',
@@ -39,6 +43,7 @@ export default {
     }
   },
   created() {
+    setInterval(() => this.test = !this.test, 2000)
     /*     setInterval(() => {
       if (this.render.length === 0)
         this.render = [
---
+++ src/components/View/Tasks/Checklist/Edit.vue
@@ -0,0 +1,70 @@
+
+<template>
+  <transition name="trans" appear
+    @enter='enter'
+  >
+    <div class="Edit">
+      <div class="wrapper rb">
+        asdf
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    enter(el) {
+      const cont = el.getElementsByClassName('wrapper')[0]
+      const s = el.style
+      const c = cont.style
+      const height = el.offsetHeight
+
+      c.transitionDuration = '0s'
+      s.transitionDuration = '0s'
+      s.width = 0
+      c.width = 0
+      s.height = 0
+      c.height = 0
+      setTimeout(() => {
+        c.transitionDuration = '.2s'
+        s.transitionDuration = '.2s'
+        if (height < 36) {
+          s.height = '35px'
+          c.height = '35px'
+        }
+        else {
+          s.height = height + 'px'
+          c.height = height + 'px'
+        }
+        s.width = '100%'
+        c.width = '100%'
+      })
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+.trans-enter, .trans-leave-to {
+  opacity: 0;
+}
+
+.trans-leave, .trans-enter-to {
+  opacity: 1;
+}
+
+.Edit {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.wrapper {
+  background-color: var(--void);
+}
+
+</style>
