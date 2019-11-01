66eee2b431dfc6a99d41d00cd77ff3603997d511
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-11-01 11:38:10 -0300

    final

--- tests/unit/example.spec.js
+++
@@ -1,9 +0,0 @@
-
-import { shallowMount } from '@vue/test-utils'
-import Vue from '@/App.vue'
-
-describe('Root component', () => {
-  it('Renders app', () => {
-    const msg = 'new message'
-  })
-})
