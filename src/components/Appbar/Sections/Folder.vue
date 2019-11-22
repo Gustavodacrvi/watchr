6f2bc89a75fb6d06d905e8fdb1fddf12ceed1cff
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-11-22 13:47:21 -0300

    folder apply selected

--- src/components/Appbar/Sections/Folder.vue
+++ src/components/Appbar/Sections/Folder.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="Folder AppbarElement">
     <div class="header rb cursor handle-folder AppbarElement-link"
-      @click="toggle"
+      @click="go"
       @mouseenter="headerHover = true"
       @mouseleave="headerHover = false"
 
@@ -12,7 +12,12 @@
       <span class="icon-wrapper">
         <Icon class="icon" :class="{headerHover}" icon="folder"/>
       </span>
-      <span class="name"><b>{{ name }}</b></span>
+      <span class="name-wrapper">
+        <transition name="name-t">
+          <span v-if="!showSpecialInfo" class="name" key="nam"><b>{{ name }}</b></span>
+          <span v-else key="f">{{ l['Apply selected tasks'] }}</span>
+        </transition>
+      </span>
       <IconDrop class="drop"
         handle="settings-v"
         :hideHandle="!headerHover && isDesktop"
@@ -33,7 +38,7 @@ import Icon from '@/components/Icon.vue'
 import IconDrop from '@/components/IconDrop/IconDrop.vue'
 
 import utils from "@/utils"
-import { mapGetters } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 
 export default {
   props: ['name', 'id', 'defaultShowing', 'movingFolder'],
@@ -82,6 +87,7 @@ export default {
     },
   },
   computed: {
+    ...mapState(['selectedTasks', 'isOnControl']),
     ...mapGetters(['l', 'isDesktop']),
     options() {
       return [
@@ -102,6 +108,12 @@ export default {
           callback: () => this.delete()
         },
       ]
+    },
+    isSelectingTasks() {
+      return this.selectedTasks.length > 0
+    },
+    showSpecialInfo() {
+      return this.headerHover && !this.isOnControl && this.isSelectingTasks
     },
   },
   watch: {
@@ -148,7 +160,7 @@ export default {
   height: 100%;
 }
 
-.name {
+.name-wrapper {
   display: flex;
   align-items: center;
 }
@@ -158,6 +170,26 @@ export default {
   right: 6px;
   top: 50%;
   transform: translateY(-50%);
+}
+
+.name-t-enter {
+  opacity: 0;
+  transform: translateY(-25px); 
+}
+
+.name-t-enter-active, .name-t-leave-active {
+  position: absolute;
+  transition-duration: .15s;
+}
+
+.name-t-enter-to, .name-t-leave {
+  transform: translateY(0px);
+  opacity: 1;
+}
+
+.name-t-leave-to {
+  opacity: 0;
+  transform: translateY(25px);
 }
 
 </style>
