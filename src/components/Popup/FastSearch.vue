34b9db52beab200dcca1cf81b3ea360c2b15fed1
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-12-06 16:47:39 -0300

    search for smart views

--- src/components/Popup/FastSearch.vue
+++ src/components/Popup/FastSearch.vue
@@ -82,7 +82,7 @@ export default {
       if (callback) callback()
     },
     getOptions() {
-      const { search, tasks, tags, lists, folders } = this
+      const { search, tasks, tags, lists, folders, views } = this
       if (!search) return []
       const lower = search.toLowerCase()
       const arr = []
@@ -93,9 +93,16 @@ export default {
       const lt = filter(lists)
       const ts = filter(tasks)
       const fs = filter(folders)
+      const vs = filter(views)
 
       const go = (route) => this.$router.push(route)
 
+      for (const v of vs)
+        arr.push({
+          ...v,
+          id: v.name + 'UNIQUE_SMART_VIEW' + Date.now(),
+          callback: () => go('/user?list=' + v.name)
+        })
       for (const t of tg)
         arr.push({
           name: t.name,
@@ -166,9 +173,43 @@ export default {
       folders: state => state.folder.folders,
     }),
     ...mapGetters(['isStandAlone']),
-  },
-  noActive() {
-    return !this.active.name || !this.active.icon
+    views() {
+      return [
+        {
+          name: 'Today',
+          color: 'var(--yellow)',
+          icon: 'star',
+        },
+        {
+          name: 'Tomorrow',
+          color: 'var(--orange)',
+          icon: 'sun',
+        },
+        {
+          name: 'Someday',
+          color: 'var(--brown)',
+          icon: 'archive',
+        },
+        {
+          name: 'Inbox',
+          color: 'var(--primary)',
+          icon: 'inbox',
+        },
+        {
+          name: 'Upcoming',
+          color: 'var(--green)',
+          icon: 'calendar',
+        },
+        {
+          name: 'Completed',
+          color: 'var(--olive)',
+          icon: 'circle-check',
+        },
+      ]
+    },
+    noActive() {
+      return !this.active.name || !this.active.icon
+    },
   },
   watch: {
     search() {
