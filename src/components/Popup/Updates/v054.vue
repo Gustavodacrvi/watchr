7c9a1794e32b2ba401dbd738558842afd00a18a8
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-12-12 20:34:12 -0300

    bug

--- src/components/Popup/Updates/v054.vue
+++ src/components/Popup/Updates/v054.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <h4>Changes</h4>
+    <ul>
       <li>It's now possible to change the initial view by sorting the smart views using drag and drop, by the default, the initial view is the Today.</li>
       <li>Much better task add UI, especially on mobile.</li>
+    </ul>
     <h4>Improvements</h4>
     <ul>
       <li>Complete app restructuring and better caching systems causing an signficant improvement in performance.</li>
-      <li>Bug fixes.</li>
+    </ul>
+    <h4>Bug fixes</h4>
+    <ul>
+      <li>Bug when selecting null dates for lists.</li>
     </ul>
   </div>
 </template>
