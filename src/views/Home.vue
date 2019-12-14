f1fdb0968320893088e89c83c8ae38999c58fd93
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-12-13 23:49:50 -0300

    final

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
-    <Error/>
   </div>
 </template>
 
 <script>
 
-import Error from '@/components/Illustrations/ErrorComponent.vue'
-
 export default {
-  components: {Error}
+
 }
+
 </script>
