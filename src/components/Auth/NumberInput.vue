a7fe7ff7677b103a214115d50e47be64bdd9e089
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-12-17 18:51:38 -0300

    Number input

---
+++ src/components/Auth/NumberInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <input class="SimpleInput cbd rb"
+    autocomplete="off"
+    type="text"
+    :value="str"
+    @input="v => str = v.target.value"
+    onkeydown="this.style.width = ((this.value.length + 1) * 12) + 'px';"
+  />
+</template>
+
+<script>
+
+export default {
+  props: ['value'],
+  data() {
+    return {
+      str: this.value + '',
+    }
+  },
+  watch: {
+    str() {
+      this.$emit('input', parseInt(this.str, 10))
+    },
+    value() {
+      this.str = this.value + ''
+    }
+  }  
+}
+
+</script>
+
+<style scoped>
+
+.SimpleInput {
+  padding: 4px;
+  font-size: 1em;
+  outline: none;
+  width: 15px;
+  max-width: 100px;
+}
+
+</style>
--- src/components/IconDrop/Calendar/Repeat/RepeatPicker.vue
+++ src/components/IconDrop/Calendar/Repeat/RepeatPicker.vue
@@ -7,21 +7,32 @@
       minWidth='200px'
       @select='v => activeRepeatOption = v'
     />
+    <div class="hr"></div>
+    <div>
+      Repeat
+      <AuthNumberInput
+        v-model="days"
+        width='15px'
+      />
+       days after task completion.
+    </div>
   </div>
 </template>
 
 <script>
 
 import AuthOptions from "@/components/Auth/Options.vue"
+import AuthNumberInput from "@/components/Auth/NumberInput.vue"
 
 import { mapGetters } from 'vuex'
 
 export default {
   components: {
-    AuthOptions,
+    AuthOptions, AuthNumberInput,
   },
   data() {
     return {
+      days: 1,
       activeRepeatOption: 'After completion',
       repeatOptions: [
         'After completion',
@@ -42,10 +53,15 @@ export default {
 <style scoped>
 
 .RepeatPicker.desktop {
-  flex-basis: 400px;
-  width: 400px;
+  flex-basis: 450px;
+  width: 450px;
   margin: 12px;
   padding: 0 12px;
+}
+
+.hr {
+  margin: 12px 0;
+  border: 1px solid var(--dark);
 }
 
 </style>
