0503d768752d6afbb153d7dab711a37bcbe334bd
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-12-25 18:47:11 -0300

    final

---
+++ src/components/Popup/Updates/v064.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h3>Better Upcoming, Today, Tomorrow Integration</h3>
+    
+  <br>
+    <span>Before this update, it wasn't possible to plan out the week using "Tomorrow" or "Upcoming", let's say you had these tasks on "Tomorrow": 'a', 'b', 'c', and you rearranged them to: 'a', 'c', 'b', on next day, the new order wouldn't be used for the Today, and instead of 'a', 'c', 'b', you would see 'a', 'b', 'c', the same used to be the case for Upcoming headings, which doesn't make sense.</span>
+    <br><br>
+    <span>This update integrates these views better, for now on, you will be able to plan out your week using Upcoming or Tomorrow, the order will then be used in "Today", functioning exactly as it should.</span>
+  </div>
+</template>
--- src/store/index.js
+++ src/store/index.js
@@ -57,7 +57,7 @@ moment.locale(lang)
 const uid = () => auth.currentUser.uid
 
 
-const version = '063'
+const version = '064'
 
 let lastVersion = localStorage.getItem('watchr_version')
 
