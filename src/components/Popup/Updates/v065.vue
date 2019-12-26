fa2824a79b51a4308197ba1a71be4fabbb55cffd
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-12-26 16:33:19 -0300

    update component

---
+++ src/components/Popup/Updates/v065.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h3>New Calendar Smart View</h3>
+    <br>
+
+    <li>You can now easily plan the week using the "Calendar" smart view.</li>
+    <li>It's also possible to re-schedule tasks by selecting them and then clicking on one of the calendar dates in the "Calendar" smart view.</li>
+    <li>New task icon when selecting tasks.</li>
+
+  </div>
+</template>
