4ca07f1ba3ecaa222d342854a92164fb5269f8fb
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-12-30 22:30:05 -0300

    final update

---
+++ src/components/Popup/Updates/v070.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h3>Pomodoro Options</h3>
+    <br>
+
+    <li>Change pomodoro duration time and long, short intervals.</li>
+
+    <h3>Bug Fixes</h3>
+
+    <li>Couldn't search smart view on mobile.</li>
+    <li>Pomodoro restarting when completing a task.</li>
+
+  </div>
+</template>
