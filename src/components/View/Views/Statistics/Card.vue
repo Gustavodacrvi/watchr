3ac5314479098622a2bb4f932f5b4890c477e891
Gustavodacrvi <gustavodacrvi@gmail.com>
2020-01-01 19:06:35 -0300

    statistics Card

---
+++ src/components/View/Views/Statistics/Card.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="Card cursor remove-highlight shadow cb">
+    <Icon class="icon"
+      :icon='icon'
+      color='var(--gray)'
+      width='150px'
+    />
+    <div class="wrapper">
+      <h4>
+        {{title}}
+      </h4>
+      <div class="cont">
+        {{descr}}
+      </div>
+      <CircleBubble class="bubble"
+        innerColor='var(--light-gray)'
+        outerColor='var(--gray)'
+        opacity='0'
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Icon from '@/components/Icon.vue'
+
+export default {
+  components: {
+    Icon,
+  },
+  props: ['title', 'descr', 'icon', 'color']
+}
+
+</script>
+
+<style scoped>
+
+.Card {
+  position: relative;
+  display: inline-block;
+  flex-basis: 336px;
+  transform: translateY(0px);
+  transition-duration: .2s;
+  overflow: hidden;
+  border-radius: 14px;
+}
+
+.wrapper {
+  padding: 24px;
+  position: relative;
+  overflow: hidden;
+  transition-duration: .2s;
+}
+
+.cont {
+  margin-top: 16px;
+}
+
+.icon {
+  position: absolute;
+  left: -20px;
+  top: -30px;
+  opacity: .1;
+}
+
+.Card:hover {
+  transform: translateY(-3px);
+  background-color: var(--light-gray);
+}
+
+</style>
--- src/components/View/Views/Statistics/Statistics.vue
+++ src/components/View/Views/Statistics/Statistics.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="Statistics">
-    Statistics
+    <h3 class="title">Pomodoro</h3>
+    <div class="wrapper">
+      <Card
+        title='Recent Data'
+        descr='Predict the efficiency changes by comparing your activity in a certain period of time.'
+        icon='pomo'
+        color='var(--dark-red)'
+      />
+    </div>
   </div>
 </template>
 
 <script>
 
+import Card from './Card.vue'
+
 export default {
-  
+  components: {
+    Card,
+  },
 }
 
 </script>
 
 <style scoped>
+
+.wrapper {
+  display: flex;
+}
+
+.title {
+  margin: 18px;
+}
 
 </style>
