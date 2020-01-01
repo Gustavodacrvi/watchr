c9e056ceeb5e7934c2a7e307f6c67ec1faf72fdc
Gustavodacrvi <gustavodacrvi@gmail.com>
2020-01-01 19:56:48 -0300

    basic data css

--- src/components/Popup/Statistics/RecentData.vue
+++ src/components/Popup/Statistics/RecentData.vue
@@ -1,7 +1,89 @@
 <template>
-  <div class="RecentData popup scroll-thin cb shadow rb">
+  <div class="RecentData popup scroll-thin cb shadow rb" :class="platform">
     <div class="tac title">
       <h3 class="pc">{{ l['Recent Data'] }}</h3>
+    </div>
+    <div class="cont">
+      <div class="numbers">
+        <div class="row">
+          <div class="cell">
+            <span class="num">
+              0
+            </span>
+            <span class="label">
+              Daily Pomos
+            </span>
+          </div>
+          <div class="cell">
+            <span class="num">
+              0
+            </span>
+            <span class="label">
+              Weekly Pomos
+            </span>
+          </div>
+          <div class="cell">
+            <span class="num">
+              0
+            </span>
+            <span class="label">
+              Total Pomos
+            </span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="cell">
+            <span class="num">
+              0
+            </span>
+            <span class="label">
+              Daily Focus(h)
+            </span>
+          </div>
+          <div class="cell">
+            <span class="num">
+              0
+            </span>
+            <span class="label">
+              Weekly Focus(h)
+            </span>
+          </div>
+          <div class="cell">
+            <span class="num">
+              0
+            </span>
+            <span class="label">
+              Total Focus(h)
+            </span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="cell">
+            <span class="num">
+              0
+            </span>
+            <span class="label">
+              Daily Rest(h)
+            </span>
+          </div>
+          <div class="cell">
+            <span class="num">
+              0
+            </span>
+            <span class="label">
+              Weekly Rest(h)
+            </span>
+          </div>
+          <div class="cell">
+            <span class="num">
+              0
+            </span>
+            <span class="label">
+              Total Rest(h)
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,7 +94,7 @@ import { mapGetters } from 'vuex'
 
 export default {
   computed: {
-    ...mapGetters(['l']),
+    ...mapGetters(['l', 'platform']),
   },
 }
 
@@ -24,7 +106,51 @@ export default {
 <style scoped>
 
 .RecentData {
-  overflow: hidden;
+  overflow: auto;
+}
+
+.desktop {
+  max-height: 500px;
+}
+
+.cont {
+  margin: 18px;
+  margin-top: 0;
+}
+
+.numbers {
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  flex-wrap: nowrap;
+}
+
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: nowrap;
+}
+
+.cell {
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  height: 70px;
+  flex-basis: 130px;
+  text-align: center;
+}
+
+.num {
+  font-size: 2em;
+}
+
+.label {
+  font-size: .8em;
+}
+
+.mobile .label {
+  font-size: .65em;
 }
 
 </style>
