0e5419bbd39b1de68372f28397b9c346718e06ec
Gustavodacrvi <gustavodacrvi@gmail.com>
2020-01-10 19:50:03 -0300

    volume slider design

---
+++ src/components/View/Views/Pomodoro/VolumeSlider.vue
@@ -0,0 +1,52 @@
+<template>
+  <DotSlider class="VolumeSlider"
+    v-model="value"
+    width='125px'
+  />
+</template>
+
+<script>
+
+import DotSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
+
+export default {
+  components: {
+    DotSlider,
+  },
+  data() {
+    return {
+      value: 50,
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+
+.VolumeSlider {
+  position: relative;
+  display: inline-block;
+  transform: translateY(-3px);
+  margin-left: 4px;
+}
+
+</style>
+
+<style>
+
+.vue-slider-rail {
+  background-color: var(--light-gray) !important;
+}
+
+.vue-slider-process {
+  background-color: var(--gray) !important;
+}
+
+.vue-slider-dot-handle {
+  background-color: var(--primary) !important;
+  border: none !important;
+}
+
+</style>
