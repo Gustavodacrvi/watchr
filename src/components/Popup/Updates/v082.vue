d0c995ac9751c8660cfa7abd2ec9ab3ddca2ab93
Gustavodacrvi <gustavodacrvi@gmail.com>
2020-01-18 17:00:24 -0300

    final

--- src/components/Popup/Updates/v082.vue
+++ src/components/Popup/Updates/v082.vue
@@ -8,5 +8,20 @@
     <li>Lists can also have someday dates.</li>
     <li>List deadlines.</li>
 
+
+    <br>
+    <h4>Repeat lists</h4>
+    <br>
+
+    <li>Lists can also repeat themselves just like tasks, they work differently though.</li>
+
+    <br>
+    <li><strong>Specific dates: </strong>
+      On tasks, specific dates represent the day when the task should be completed, that is, if set to the 24th of January, the task should be completed that day, otherwise it goes to overdue. In lists, however, it works more like defer dates, or the day when the list will be ready to be worked on. The lists would then only be shown on the menu on the 24th day of January and would continue to be there until its completion.
+    </li>
+    <br>
+
+    <li><strong>After completion, daily, weekly and yearly:</strong> You can also use complex recurring dates in lists, the recuring dates restores the lists, resetting all of its tasks and showing the list again on menu, they never go to overdue, and will sit there waiting to be checked off, once completed, it restores itself on the next recurring event, for that reason, you can't add daily recurring dates to lists, because it would work the same way as after completion dates. <br><br> Example: Let's say you create a recurring list on the second Thursday of every month, once completing the list, it disappears from the menu, it will be ready to be completed again on the next month, all of its tasks will be uncompleted, restoring the list..</li>
+
   </div>
 </template>
