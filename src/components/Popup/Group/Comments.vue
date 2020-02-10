09eea82706d63363c248de88a0e3e953e8bc30b7
Gustavodacrvi <gustavodacrvi@gmail.com>
2020-02-09 21:32:28 -0300

    editor container

--- src/components/Popup/Group/Comments.vue
+++ src/components/Popup/Group/Comments.vue
@@ -5,10 +5,39 @@
   >
     <div class="wrapper">
       <div class="messages">
-        fsd
+        <Icon
+          class="comment-background"
+          icon='comment'
+          color='var(--sidebar-color)'
+          width='150px'
+        />
       </div>
-      <div class="editor">
-        asdf
+      <div class="editor rb">
+        <div class="text">
+          <textarea
+            ref='input'
+            placeholder="Comment..."
+            class="txt scroll-thin"
+            cols="2"
+          >
+          </textarea>
+        </div>
+        <div class="header">
+          <div class="tools">
+            <Icon
+              class="cursor remove-highlight primary-hover"
+              icon='file'
+              title="Add file"
+            />
+          </div>
+          <div class="btn">
+            <button
+              class="button rb"
+            >
+              Add comment
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -16,10 +45,26 @@
 
 <script>
 
+import Icon from "@/components/Icon.vue"
+import AuthButton from "@/components/Auth/Button.vue"
+
 import { mapGetters } from 'vuex'
 
 export default {
+  components: {
+    Icon, AuthButton,
+  },
   props: ['payload'],
+  mounted() {
+    this.focus()
+  },
+  methods: {
+    focus() {
+      const inp = this.$refs.input
+      if (inp)
+        inp.focus()
+    },
+  },
   computed: {
     ...mapGetters({
       platform: 'platform',
@@ -57,10 +102,52 @@ export default {
 
 .messages {
   flex-basis: 100%;
+  position: relative;
 }
 
 .editor {
-  flex-basis: 160px;
+  border: 2px solid var(--sidebar-color);
+  display: flex;
+  flex-direction: column;
+  padding: 0 13px;
+}
+
+.text {
+  padding: 8px 0;
+  box-sizing: border-box;
+}
+
+.header {
+  flex-basis: 45px;
+  border-top: 2px solid var(--sidebar-color);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.txt {
+  width: 100%;
+  height: 100%;
+  resize: none;
+  outline: none;
+}
+
+.comment-background {
+  position: absolute;
+  left: 50%;
+  top: 55%;
+  transform: translate(-50%,-50%);
+  opacity: .6;
+}
+
+.button {
+  background-color: var(--sidebar-color);
+  padding: 8px;
+  transition-duration: .2s;
+}
+
+.button:hover {
+  background-color: var(--light-gray);
 }
 
 </style>
