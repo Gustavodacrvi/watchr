ca6a03f3872b639a937cf5d5429b5740623772d9
Gustavodacrvi <gustavodacrvi@gmail.com>
2020-03-07 11:59:56 -0300

    SectionVideo.vue

---
+++ src/components/Home/SectionVideo.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="SectionVideo">
+    <div class="section">
+      <div class="section-main-heading">
+        <h2>Magic Button</h2>
+      </div>
+
+      <div class="section-wrapper">
+        <div class="section-cont">
+          <div v-for="o in cont"
+            class="section-section"
+            :key="o.heading"
+          >
+            <div class="section-heading">
+              <h3>{{ o.heading }}</h3>
+            </div>
+            <p class="section-paragraph">
+              {{ o.txt }}
+            </p>
+          </div>
+        </div>
+        <div class="section-cont">
+          right section
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['cont']
+}
+
+</script>
+
+<style scoped>
+
+.SectionVideo {
+  display: flex;
+  margin-top: 7em;
+  justify-content: center;
+}
+
+.section {
+  flex-basis: 1100px;
+  margin: 0 12px;
+}
+
+.section-main-heading {
+  font-size: 1.35em;
+  margin: 3.5em 0;
+  text-align: center;
+}
+
+.section-heading {
+  font-size: 1.2em;
+  margin: 2.5em 0;
+}
+
+.section-wrapper {
+  display: flex;
+  height: 400px;
+}
+
+.section-cont {
+  flex-basis: 100%;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
           </div>
           <div class="hero-paragraph">
             <p>
-              An Open Source task manager software, built with Firebase and  inspired by the most popular todo apps. Capture, Organize and complete all of your tasks for only $5/year.
+              An Open Source PWA task manager software, built with Firebase and  inspired by the most popular todo apps. Capture, Organize and complete all of your tasks for only $5/year.
             </p>
           </div>
           <div class="hero-button">
@@ -18,21 +18,78 @@
         </div>
       </div>
 
-      <img id='back' src="/svg/background.svg" alt="background">
+      <div id="main-illustration-wrapper-wrapper">
+        <div id="main-illustration-wrapper">
+          <img id='main-illustration' src="/svg/main_illustration.svg" alt="background">
+        </div>
+      </div>
+
+      <SectionVideo
+        :cont="[
+          {
+            heading: 'Insert todos anywhere',
+            txt: 'You not only can tap the magic button to add tasks, you can drag and drop it anywhere to add a task exactly where it should be!',
+          },
+          {
+            heading: 'Adding headings',
+            txt: `Headings are used to organize and visually separate your stuff more easily, you can use the magic button to add a list heading anywhere.
+
+            Simply drop it to the left margin and type in its name, all of the tasks below the target will then be inside the created heading.`,
+          },
+          {
+            heading: 'Add tags and lists',
+            txt: `The magic button can also be used to add lists directly inside its folder, tags and subtags.`,
+          },
+        ]"
+      />
+
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
+      <p>asdfasdf</p>
     </div>
-    
   </main>
 </template>
 
 <script>
 
-import AuthButton from '@/components/Auth/Button.vue'
-import Info from "@/components/Home/Info.vue"
+import SectionVideo from "@/components/Home/SectionVideo.vue"
 
 export default {
   components: {
-    AuthButton,
-    Info,
+    SectionVideo,
   },
 }
 
@@ -40,10 +97,25 @@ export default {
 
 <style scoped>
 
-#back {
+#main-illustration-wrapper-wrapper {
+  display: flex;
+  justify-content: center;
+}
+
+#main-illustration-wrapper {
+  flex-basis: 85%;
+}
+
+#main-illustration {
   width: 100%;
   margin-top: 2em;
   z-index: -1;
+}
+
+p {
+  font-size: 1.1em;
+  line-height: 18px;
+  color: var(--dark-txt);
 }
 
 .Home {
@@ -111,7 +183,6 @@ export default {
 
 .hero-paragraph {
   margin: 40px 0px;
-  line-height: 18px;
 }
 
 </style>
